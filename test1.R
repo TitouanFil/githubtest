10e6f86e2a91f50ced269807ea7c1143325d060e
Titouan Filloux <titouan.filloux@wanadoo.fr>
2023-02-01 13:03:49 +0700

    On vérifie que les données soient "propres", les NA et les outliers via des commandes et des graphiques

--- test1.R
+++ test1.R
@@ -7,3 +7,13 @@ library(multcompView)
 
 #On importe un jeu de données par defaut
 data("iris")
+
+#On vérifie la distribution générale des données
+summary(iris)
+#On vérifie les NA dans la base de donnée
+which(is.na(iris))
+#On vérifie les outliers via des boxplots - Sepal.Length
+ggplot(iris, aes(x=Sepal.Length))+
+  geom_bar()+
+  scale_y_continuous(breaks=seq(0,10,1))
+
