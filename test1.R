b7929c932a673473bd4ff3b50a22d5f4da4880ad
Titouan Filloux <titouan.filloux@wanadoo.fr>
2023-02-01 11:47:01 +0700

    J'importe la database (iris database par défaut) sans aucune modification pour l'instant

---
+++ test1.R
@@ -0,0 +1,9 @@
+#On charge les packages requis
+library(lmerTest)
+library(emmeans)
+library(ggplot2)
+library(multcomp)
+library(multcompView)
+
+#On importe un jeu de données par defaut
+data("iris")
